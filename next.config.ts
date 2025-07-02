d7f2d94275e21fdd411dac743f50d7934f550fe2
jhkotoba <jhkotoba@gmail.com>
2025-07-02 14:32:36 +0900

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: '/dashboard',
+  env: {
+    GATEWAY_PREFIX: 'dashboard'
+  }
 };
 
 export default nextConfig;
